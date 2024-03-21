3c1fdbcee5e8339ee02ce978699f4f309ac965e4
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-03-21 15:22:07 -0500

    feat: :sparkles: Header almost completed
    Need to add the dropdown menu function with javascript.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const menuDesplegable = getElementById('menu__desplegable');
+const menu = document.getElementById('menu');
+const linkMenu1 = document.getElementById('menu__link1');
+const linkMenu2 = document.getElementById('menu__link2');
+const linkMenu3 = document.getElementById('menu__link3');
+const linkMenu4 = document.getElementById('menu__link4');
+const linkMenu5 = document.getElementById('menu__link5');
+const linkMenu6 = document.getElementById('menu__link6');
+const linkMenu7 = document.getElementById('menu__link7');
+const linkMenu8 = document.getElementById('menu__link8');
+const linkMenu9 = document.getElementById('menu__link9');
+const linkMenu10 = document.getElementById('menu__link10');
+const linkMenu11 = document.getElementById('menu__link11');
+const linkMenu12 = document.getElementById('menu__link12');
+const linkMenu13 = document.getElementById('menu__link13');
+const linkMenu14 = document.getElementById('menu__link14');
+const linkMenu15 = document.getElementById('menu__link15');
+
+
+menuDesplegable.addEventListener('click', () => {
+    menu.classList.toggle('menu__show');
+});
